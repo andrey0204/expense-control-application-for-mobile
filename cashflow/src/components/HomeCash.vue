ff072ded0731df0bc4216c08f9887b405cbe2548
Andrey Macias <carlos.macias@inoutdelivery.com>
2023-08-03 19:36:31 -0500

    add components MovementsCash and ResumeCash

--- cashflow/src/components/HomeCash.vue
+++ cashflow/src/components/HomeCash.vue
@@ -3,17 +3,27 @@
     <template #header>
       <HeaderCash></HeaderCash>
     </template>
+    <template #resume>
+      <ResumeCash></ResumeCash>
+    </template>
+    <template #movements>
+      <MovementsCash></MovementsCash>
+    </template>
   </LayoutCash>
 </template>
 
 <script>
 import LayoutCash from "./LayoutCash.vue";
 import HeaderCash from "./HeaderCash.vue";
+import ResumeCash from "./ResumeCash.vue";
+import MovementsCash from "./MovementsCash.vue";
 
 export default {
   components: {
     LayoutCash,
     HeaderCash,
+    ResumeCash,
+    MovementsCash,
   },
 };
 </script>
---
+++ cashflow/src/components/ResumeCash.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>resume</main>
+</template>
+<style scope>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+h1 {
+  margin-top: 14px;
+  color: var(--brand-green);
+}
+.graphic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 48px 24px;
+  box-sizing: border-box;
+}
+</style>
